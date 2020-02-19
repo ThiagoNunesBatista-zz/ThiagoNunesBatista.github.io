0150c5ee22a1ee65b2a2c6a63f95353294d27f50
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-02-19 10:40:26 -0300

    Change URL

--- projectCourses/githubFinder/api.js
+++ projectCourses/githubFinder/api.js
@@ -1,15 +1,20 @@
 class GitHubApi {
   constructor() {
     this.baseUrl = 'https://api.github.com'
+    this.perPage = 2
+    this.page = 0
   }
 
   async get(username) {
-    const result = await fetch(`${this.baseUrl}/users/${username}`)
+    const url = `${this.baseUrl}/users/${username}`
+    const result = await fetch(url)
     return result.json()
   }
 
   async getRepositories(username) {
-    const result = await fetch(`${this.baseUrl}/users/${username}/repos`)
+    const url = `${this.baseUrl}/users/${username}/repos?page=${this.page}&per_page=${this.perPage}`
+    this.page++;
+    const result = await fetch(url)
     return result.json()
   }
 }
